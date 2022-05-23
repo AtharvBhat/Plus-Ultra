90edb2911476a69f7ba36562382edcdd3c33dfba
Atharv Bhat <bhat.atharv@gmail.com>
2022-05-23 14:30:50 -0400

    Added Wandb Integration

--- train.py
+++ train.py
@@ -5,29 +5,45 @@ import transforms as T
 from dataset import SRDataset
 import matplotlib.pyplot as plt
 from model import AttU_Net2x
-from tqdm import tqdm
 from utils import train_one_epoch
+import wandb
 
 if __name__ == "__main__":
-        
+    
+    #init wandb
+    wandb.init(project="Plus-Ultra", entity="atharvbhat")
+
     #hyper params
     batch_size = 8
     lr = 1e-4
+    num_epochs = 10
 
-    transforms = torchvision.transforms.Compose([T.JpegCorrupt(1, (10, 100)),
+    transforms = torchvision.transforms.Compose([T.JpegCorrupt(0.5, (10, 100)),
                                                 T.ToTensor(),
                                                 T.RandomCrop(512),
                                                 T.ResizeX()])
 
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    print(device)
 
-    dataset = SRDataset("data/images", transforms=transforms)
+    dataset = SRDataset("data/train_images", transforms=transforms)
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=8)
     model = AttU_Net2x()
     model = model.to(device)
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr = lr)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, lr, epochs=num_epochs, steps_per_epoch=len(train_loader))
 
-    train_one_epoch(model, criterion, train_loader, device, optimizer)
+    wandb.watch(model, criterion, "all", 50, log_graph=True)
+
+    best_loss = None
+    for i in range(num_epochs):
+        epoch_loss = train_one_epoch(model, criterion, train_loader, device, optimizer, scheduler, i)
+        wandb.log({"Epoch Loss" : epoch_loss, "epoch" : i})
+        if best_loss is not None and epoch_loss < best_loss:
+            torch.save(model.state_dict(), "checkpoints/best_model.pth")
+        else:
+            torch.save(model.state_dict(), "checkpoints/best_model.pth")
+            best_loss = epoch_loss
+
+    wandb.finish()
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from PIL import Image
 import cv2
 import numpy as np
 from tqdm import tqdm
+import wandb
 
 def pil_to_cv2(pil_image):
     """
@@ -27,16 +28,25 @@ def jpeg_compress(cv2_img, quality):
     compressed_img = cv2.imdecode(jpeg_encode, cv2.IMREAD_UNCHANGED)
     return compressed_img
 
-def train_one_epoch(model, criterion, train_loader, device, optimizer):
-    for i, sample in tqdm(enumerate(train_loader)):
+def train_one_epoch(model, criterion, train_loader, device, optimizer, scheduler, epoch, log_step=50):
+    """
+    Given a model, criterion, dataloader device, optimizer and scheduler
+    runs one epoch of training
+    """
+    avg_loss = 0
+    i=0
+    for sample in tqdm(train_loader):
         optimizer.zero_grad()
         x, y = sample["x"], sample["y"]
         x, y = x.to(device), y.to(device)
-
         y_pred = model(x)
-
         loss = criterion(y_pred, y)
         loss.backward()
         optimizer.step()
-        if i%1 == 0:
-            print(loss.item())
+        scheduler.step()
+        if i%log_step == 0:
+            wandb.log({"Step Loss" : loss.item(), "lr" : optimizer.param_groups[0]['lr']})
+        avg_loss += loss.item()
+        i+=1
+    return avg_loss/i
+    
