2defc31e492eb3a4786044ed2510f86d7670f545
Atharv Bhat <bhat.atharv@gmail.com>
2023-10-09 17:20:26 -0400

    setup precommit

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+# flake8: noqa
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='A project to enhance digital media using deep learning',
-    author='Atharv Bhat',
-    license='MIT',
+    version="0.1.0",
+    description="A project to enhance digital media using deep learning",
+    author="Atharv Bhat",
+    license="MIT",
 )
